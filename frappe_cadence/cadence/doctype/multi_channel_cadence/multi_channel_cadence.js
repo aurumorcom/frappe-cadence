132b207efc49a6c38a9eace82a07190a6f25b33f
Aryan Singh <aquiveal@gmail.com>
2026-08-06 23:06:30 +0530

    fix: update cadence form name and filters
    Change the form name from "Email Cadence" to "Multi Channel Cadence"
    to match the actual doctype. Add include_disabled filter to the
    reference_name query to allow selection of disabled records in
    cadence schedules.

--- frappe_cadence/cadence/doctype/multi_channel_cadence/multi_channel_cadence.js
+++ frappe_cadence/cadence/doctype/multi_channel_cadence/multi_channel_cadence.js
@@ -1,11 +1,13 @@
 // Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Email Cadence", {
+frappe.ui.form.on("Multi Channel Cadence", {
 	setup: function(frm) {
 		frm.set_query("reference_name", "cadence_schedules", function(doc, cdt, cdn) {
 			return {
-				filters: {}
+				filters: {
+					include_disabled: 1
+				}
 			};
 		});
 	},
