c9faf6e5a42f732973393a5a8c319dcf9df47925
Aryan Singh <aquiveal@gmail.com>
2026-08-06 23:20:55 +0530

    refactor: simplify cadence field handler
    Remove the setup function that set query filters for reference_name
    field as it's no longer needed. Rename email_cadence_for handler to
    cadence_for to better reflect its generic purpose across all cadence
    types.

--- frappe_cadence/cadence/doctype/multi_channel_cadence/multi_channel_cadence.js
+++ frappe_cadence/cadence/doctype/multi_channel_cadence/multi_channel_cadence.js
@@ -2,16 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Multi Channel Cadence", {
-	setup: function(frm) {
-		frm.set_query("reference_name", "cadence_schedules", function(doc, cdt, cdn) {
-			return {
-				filters: {
-					include_disabled: 1
-				}
-			};
-		});
-	},
-	email_cadence_for: function (frm) {
+	cadence_for: function (frm) {
 		frm.set_value("recipient", "");
 	},
 });
