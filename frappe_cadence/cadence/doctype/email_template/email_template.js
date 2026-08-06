193ede39b8c64ec24ee610ee67b16618440b62f5
Aryan Singh <aquiveal@gmail.com>
2026-08-06 15:04:20 +0530

    refactor: move sift module to integrations
    Update all references to sift module from frappe_cadence.utils
    to frappe_cadence.integrations to reflect the new module location.
    
    This includes updating method paths in email, SMS, LinkedIn, and
    WhatsApp template doctypes, as well as test cassette fixtures.

--- frappe_cadence/cadence/doctype/email_template/email_template.js
+++ frappe_cadence/cadence/doctype/email_template/email_template.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Email Template", {
 	refresh: function(frm) {
 		frm.add_custom_button(__("Optimize"), function() {
 			frappe.call({
-				method: "frappe_cadence.utils.sift.optimize",
+				method: "frappe_cadence.integrations.sift.optimize",
 				args: {
 					template_doctype: frm.doc.doctype,
 					template_name: frm.doc.name
@@ -16,7 +16,7 @@ frappe.ui.form.on("Email Template", {
 		});
 		frm.add_custom_button(__("Predict"), function() {
 			frappe.call({
-				method: "frappe_cadence.utils.sift.predict",
+				method: "frappe_cadence.integrations.sift.predict",
 				args: {
 					template_doctype: frm.doc.doctype,
 					template_name: frm.doc.name
