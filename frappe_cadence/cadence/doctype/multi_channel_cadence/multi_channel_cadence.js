09b670862ea40ce0f47478264ec89af58a32ea91
Aryan Singh <aquiveal@gmail.com>
2026-08-06 22:47:41 +0530

    feat: add setup hook for cadence schedules
    Add a setup function to initialize query filters for the
    reference_name field in cadence_schedules child table. This
    prepares the groundwork for dynamic filtering based on the
    selected email cadence type.

--- frappe_cadence/cadence/doctype/multi_channel_cadence/multi_channel_cadence.js
+++ frappe_cadence/cadence/doctype/multi_channel_cadence/multi_channel_cadence.js
@@ -2,6 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Email Cadence", {
+	setup: function(frm) {
+		frm.set_query("reference_name", "cadence_schedules", function(doc, cdt, cdn) {
+			return {
+				filters: {}
+			};
+		});
+	},
 	email_cadence_for: function (frm) {
 		frm.set_value("recipient", "");
 	},
